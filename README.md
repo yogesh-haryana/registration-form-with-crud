de65352244a2709af826bc56a0a14f10fcf0d8d9
yogesh-haryana <personalyogesh352@gmail.com>
2022-09-26 15:12:55 +0530

    UI Updated

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
-This is a basic registration form with multiple type of input fields with validation.
-it is written in pure javascript which have the CRUD operation functionalities.
+# How to use=>
 
-## How to use=>
+## Setup Project
 
-step1 >>  Get a clone of the Repository.
-step2 >>  open it with your editor.
-step3 >>  Launch it with Live Server.
-step4 >>  Fill the form and play with CRUD operations.
+Clone the application from `git remote add origin https://github.com/yogesh-haryana/registration-form-with-crud.git`
+
+
+### Install LiveServer Extension
+
+`live server`
+
+
+### Launch the Live Server
+
+
+Code is now running on port 5500.
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 recordInTheTable();
+const modalbox = document.getElementById("modal-box");
 function openTab(event, buttonName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -11,33 +12,93 @@ function openTab(event, buttonName) {
     }
     document.getElementById(buttonName).style.display = "block";
     // event.target.className += " active";
-
+    modalbox.style.display = "none";
 }
 openTab(event, 'Registration');
 
-// -------------form starts here----------------
-const emailAlert = document.getElementById("emailAlert");
-const successMsg = document.getElementById("success");
-const updateMsg = document.getElementById("update-success");
 const deleteMsg = document.getElementById("delete-success");
-const modalbox = document.getElementById("modal-box");
+const usernameMsg = document.getElementById("usernameMsg");
+const emailMsg = document.getElementById("emailMsg");
+const mobileMsg = document.getElementById("mobileMsg");
+const dobMsg = document.getElementById("dobMsg");
+const genderMsg = document.getElementById("genderMsg");
+const qualificationsMsg = document.getElementById("qualificationsMsg");
+const profileMsg = document.getElementById("profileMsg");
+const emailAlert = document.getElementById("emailalert");
+const successMsg = document.getElementById("successmsg");
+const updateMsg = document.getElementById("updatemsg");
+const saveIndex = document.getElementById("saveIndex");
+const submitbtn = document.getElementById("submitbtn");
+const updatebtn = document.getElementById("updatebtn");
+const preview = document.getElementById("preview");
+const mobileNan = document.getElementById("mobileNan");
+
+
+let username = document.studentForm.username;
+let email = document.studentForm.email;
+let mobileno = document.studentForm.mobileno;
+let dob = document.studentForm.dob;
+let gender = document.studentForm.gender;
+let qualifications = document.studentForm.qualifications;
+let profile = document.studentForm.profile;
 
 function onFormSubmit(event) {
-    let emailValidIndex = isEmailValid();
-    if (emailValidIndex === false) {
-        formData = readFormData();
-        saveToLocalStorage();
-        recordInTheTable();
-        resetForm();
-        successMsg.style.display = "block";
-        updateMsg.style.display = "none";
 
-    }
-    else {
-        emailAlert.style.display = "block";
+    if (Validate() === true) {
+        let emailValidIndex = isEmailValid();
+        if (emailValidIndex === false) {
+            formData = readFormData();
+            saveToLocalStorage();
+            recordInTheTable();
+            resetForm();
+            successMsg.style.display = "block";
+            updateMsg.style.display = "none";
+            mobileNan.style.display = "none";
+
+        }
+        else {
+            emailAlert.style.display = "block";
+        }
+
     }
     event.preventDefault();
-
+}
+// --------------------form validation---------------------
+function Validate() {
+    let validationStatus = true;
+    if (username.value === "") {
+        usernameMsg.style.display = "block";
+        validationStatus = false;
+    }
+    if (email.value === "") {
+        emailMsg.style.display = "block";
+        validationStatus = false;
+    }
+    if (mobileno.value === "") {
+        mobileMsg.style.display = "block";
+        validationStatus = false;
+    }
+    if (isNaN(mobileno.value)) {
+        mobileNan.style.display = "block";
+        validationStatus = false;
+    }
+    if (dob.value === "") {
+        dobMsg.style.display = "block";
+        validationStatus = false;
+    }
+    if (gender.value === "") {
+        genderMsg.style.display = "block";
+        validationStatus = false;
+    }
+    if (qualifications.value === "select") {
+        qualificationsMsg.style.display = "block";
+        validationStatus = false;
+    }
+    if (profile.value === "") {
+        profileMsg.style.display = "block";
+        validationStatus = false;
+    }
+    return validationStatus;
 }
 isEmailValid = () => {
     let emailValue = document.getElementById("email").value;
@@ -58,7 +119,6 @@ isEmailValid = () => {
 
 
 let genderValue = () => {
-    let gender = document.getElementsByName("gender");
 
     for (let i = 0; i < gender.length; i++) {
         if ((gender[i].checked) == true) {
@@ -67,7 +127,6 @@ let genderValue = () => {
     }
 }
 let genderValueReset = () => {
-    let gender = document.getElementsByName("gender");
 
     for (let i = 0; i < gender.length; i++) {
         gender[i].checked = false;
@@ -83,20 +142,21 @@ var uploadImage = () => {
     profileImage = (document.querySelector(`input[type="file"]`).files[0]);
     const reader = new FileReader();
     reader.addEventListener('load', function () {
-        document.getElementById("demo-image").setAttribute("src", this.result);
+        document.getElementById("preview").setAttribute("src", this.result);
     });
     reader.readAsDataURL(profileImage);
-    let profilePictureURL = document.getElementById("demo-image").getAttribute("src");
+    let profilePictureURL = document.getElementById("preview").getAttribute("src");
+    preview.style.display = "block";
     return profilePictureURL;
 
 }
 //--------binding formdata in objects ------------------------
 function readFormData() {
     var formData = {};
-    formData["username"] = document.getElementById("username").value;
-    formData["email"] = document.getElementById("email").value;
-    formData["phno"] = document.getElementById("phno").value;
-    formData["dob"] = document.getElementById("dob").value;
+    formData["username"] = username.value;
+    formData["email"] = email.value;
+    formData["mobileno"] = mobileno.value;
+    formData["dob"] = dob.value;
     formData["gender"] = genderValue();
     formData["qualifications"] = getSelectValue();
     formData["profile"] = uploadImage();
@@ -122,6 +182,7 @@ function saveToLocalStorage() {
 //---------showing essential information in tabular form with the data in localstorage
 
 function recordInTheTable() {
+    let index;
     let oldRecords = localStorage.getItem('userdata');
     if (oldRecords == null) {
         userData = [];
@@ -136,29 +197,43 @@ function recordInTheTable() {
                         <td>${index + 1}</td>
                         <td>${formData.username}</td>
                         <td>${formData.email}</td>
-                        <td><a onClick="onEdit(${index})">Edit</a> | 
-                        <a onClick="onDelete(${index})">Delete</a> | 
+                        <td><a onClick="onEdit(${index})">Edit</a> 
+                        <a onClick="onDelete(${index})">Delete</a> 
                         <a onClick="onView(${index})">View</a></td>
                  </tr>`
 
     });
     addDataToTable.innerHTML = html;
-
+    $(document).ready(function () {
+        $('#user-table').DataTable()
+    });
 }
+
 
 //---------------clearing all input fields-----------------------
 
 function resetForm() {
-    document.getElementById("username").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("phno").value = "";
-    document.getElementById("dob").value = "";
+    username.value = "";
+    email.value = "";
+    mobileno.value = "";
+    dob.value = "";
     genderValueReset();
-    document.getElementById("qualifications").value = '';
-    document.getElementById("profile").value = null;
+    qualifications.value = 'select';
+    profile.value = null;
     emailAlert.style.display = "none";
     successMsg.style.display = "none";
     updateMsg.style.display = "none";
+    profileMsg.style.display = "none";
+    genderMsg.style.display = "none";
+    qualificationsMsg.style.display = "none";
+    mobileMsg.style.display = "none";
+    mobileNan.style.display = "none";
+    dobMsg.style.display = "none";
+    emailMsg.style.display = "none";
+    usernameMsg.style.display = "none";
+    preview.style.display="none";
+
+
 }
 
 //----------------edit button ------------------------------------
@@ -168,25 +243,24 @@ function onEdit(index) {
     successMsg.style.display = "none";
     modalbox.style.display = "none";
 
-    let saveIndex = document.getElementById("saveIndex");
-    const submitbtn = document.getElementById("submitbtn");
-    const updatebtn = document.getElementById("update");
     let genderHere = document.getElementsByName("gender");
     saveIndex.value = index;
     userData = JSON.parse(localStorage.getItem('userdata'));
 
-    document.getElementById("username").value = userData[index].username;
-    document.getElementById("email").value = userData[index].email;
-    document.getElementById("phno").value = userData[index].phno;
-    document.getElementById("dob").value = userData[index].dob;
+    username.value = userData[index].username;
+    email.value = userData[index].email;
+    mobileno.value = userData[index].mobileno;
+    dob.value = userData[index].dob;
     for (let j = 0; j <= genderHere.length; j++) {
         if (genderHere[j].value === userData[index].gender) {
             genderHere[j].checked = true;
             break;
         }
     }
-    document.getElementById("qualifications").value = userData[index].qualifications;
 
+    qualifications.value = userData[index].qualifications;
+    preview.setAttribute("src", userData[index].profile);
+    preview.style.display = "block";
     submitbtn.style.display = "none";
     updatebtn.style.display = "block";
     openTab(event, 'Registration');
@@ -198,31 +272,30 @@ function onEdit(index) {
 
 function updateRecord() {
 
-    const submitbtn = document.getElementById("submitbtn");
-    const updatebtn = document.getElementById("update");
     let genderHere = document.getElementsByName("gender");
-
     userData = JSON.parse(localStorage.getItem('userdata'));
-    var saveIndex = document.getElementById("saveIndex").value;
+    let savedIndex = document.getElementById("saveIndex").value;
 
-    userData[saveIndex].username = document.getElementById("username").value;
-    userData[saveIndex].email = document.getElementById("email").value;
-    userData[saveIndex].phno = document.getElementById("phno").value;
-    userData[saveIndex].dob = document.getElementById("dob").value;
+    userData[savedIndex].username = username.value;
+    userData[savedIndex].email = email.value;
+    userData[savedIndex].mobileno = mobileno.value;
+    userData[savedIndex].dob = dob.value;
     for (let j = 0; j <= genderHere.length; j++) {
         if (genderHere[j].checked === true) {
-            userData[saveIndex].gender = genderHere[j].value;
+            userData[savedIndex].gender = genderHere[j].value;
             break;
         }
     }
-    document.getElementById("qualifications").value = userData[saveIndex].qualifications;
+    userData[savedIndex].qualifications = qualifications.value;
+    userData[savedIndex].profile = uploadImage();
     localStorage.setItem('userdata', JSON.stringify(userData));
     recordInTheTable();
     resetForm();
     submitbtn.style.display = "block";
     updatebtn.style.display = "none";
-    updateMsg.style.display = "block";
-    successMsg.style.display = "none";
+    updateMsg.style.display= "block";
+    // e.stopPropagation();
+    // preventDefault();
 
 }
 
@@ -271,7 +344,7 @@ function onView(index) {
 </tr>
 <tr>
     <td>Mobile Number : </td>
-    <td>${userData[index].phno}</td>
+    <td>${userData[index].mobileno}</td>
 </tr>
 <tr>
     <td>Date Of birth : </td>
